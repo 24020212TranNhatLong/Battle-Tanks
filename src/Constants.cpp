e7eb4d0d7d0ab258f1c5c8be5e11de21214d75a4
24020212TranNhatLong <24020212@vnu.edu.vn>
2025-04-03 19:48:13 +0700

    Update Constants.cpp

--- src/Constants.cpp
+++ src/Constants.cpp
@@ -3,4 +3,6 @@
 std :: vector<SDL_Rect> wallMap0 = {m0wall1, m0wall2, m0wall3, m0wall4, m0wall5, m0wall6, m0wall7} ; 
 std :: vector<SDL_Rect> wallMap1 = {m1wall1, m1wall2, m1wall3, m1wall4, m1wall5} ;  //boss
 std :: vector<SDL_Rect> wallMap2 = {m2wall1, m2wall2, m2wall3, m2wall4, m2wall5, m2wall6, m2wall7} ;
-std :: vector<SDL_Rect> wallMap3 = {m3wall1, m3wall2, m3wall3, m3wall4, m3wall5, m3wall6, m3wall7, m3wall8} ;  
+std :: vector<SDL_Rect> wallMap3 = {m3wall1, m3wall2, m3wall3, m3wall4, m3wall5, m3wall6, m3wall7, m3wall8} ; 
+
+bool sound = true;
